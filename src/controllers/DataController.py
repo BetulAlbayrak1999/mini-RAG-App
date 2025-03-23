49dff5614de3bde7e6abceb04bc13711c264b64a
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-03-23 18:48:21 +0300

    uploading file after generating a unique name

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,12 +1,17 @@
 from .BaseController import BaseController
 from fastapi import UploadFile
 from src.models import ResponseSignal
+from .ProjectController import ProjectController
+import re
+import os
+
 
 class DataController(BaseController):
     def __init__(self):
         super().__init__()
         self.size_scale = 1048576  # convert MB to bytes
 
+    # validate_uploaded_file
     def validate_uploaded_file(self, file: UploadFile):
 
         if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
@@ -16,3 +21,37 @@ class DataController(BaseController):
             return False, ResponseSignal.FILE_SIZE_EXCEEDED.value
 
         return True, ResponseSignal.FILE_UPLOAD_SUCCESS.value
+
+    # generate_unique_filename
+    def generate_unique_filename(self, original_filename: str, project_id: str):
+        random_key = self.generate_random_string()
+        project_path = ProjectController().get_project_path(project_id=project_id)
+
+        cleaned_filename = self.get_clean_filename(
+            original_filename=original_filename,
+        )
+
+        new_file_path = os.path.join(
+            project_path,
+            random_key + "_" + cleaned_filename,
+        )
+
+        while os.path.exists(new_file_path):
+            random_key = self.generate_random_string()
+            new_file_path = os.path.koin(
+                project_path,
+                random_key + "_" + cleaned_filename,
+            )
+
+        return new_file_path
+
+    # get_clean_filename
+    def get_clean_filename(self, original_filename: str):
+
+        # removeany special charachters, except underscore and .
+        cleaned_filename = re.sub(r"[^\w.]", "", original_filename.strip())
+
+        # replace spaces with underscore
+        cleaned_filename = cleaned_filename.replace(" ", "_")
+
+        return cleaned_filename
