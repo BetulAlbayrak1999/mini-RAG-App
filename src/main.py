bceabd012d717c647114e7e4a54c54986c41709d
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-03-23 12:24:45 +0300

    getting access to .env data from base and main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
 from src.routes import base, data
+
 
 app = FastAPI()
 app.include_router(base.base_router)
--- src/routes/base.py
+++ src/routes/base.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI, APIRouter
+import os
 
-base_router = APIRouter()
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"],
+)
 
 
 @base_router.get("/")
 def welcome():
-    return {"message": "hello again!"}
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+    test = os.getenv("OPENAI_API_KEY")
+
+    return {
+        "app_name": app_name,
+        "app_version": app_version,
+        "test": test,
+    }
