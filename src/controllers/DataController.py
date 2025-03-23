aff1b9a64f6fc5da6949cce353bed01abd1137c7
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-03-23 17:10:28 +0300

    creating ResponseSignal for responses

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,6 +1,6 @@
 from .BaseController import BaseController
 from fastapi import UploadFile
-
+from src.models import ResponseSignal
 
 class DataController(BaseController):
     def __init__(self):
@@ -10,9 +10,9 @@ class DataController(BaseController):
     def validate_uploaded_file(self, file: UploadFile):
 
         if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
-            return False, "file_type_not_supported"
+            return False, ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
 
         if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
-            return False, "file_size_exceeded"
+            return False, ResponseSignal.FILE_SIZE_EXCEEDED.value
 
-        return True, "success"
+        return True, ResponseSignal.FILE_UPLOAD_SUCCESS.value
