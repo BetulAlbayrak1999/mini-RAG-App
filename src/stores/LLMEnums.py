6066ceae7fd1d46707f6053627e89ddacdca1dd7
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-03-31 10:48:30 +0300

    adding OpenAIEnums class

--- src/stores/LLMEnums.py
+++ src/stores/LLMEnums.py
@@ -4,3 +4,9 @@ from enum import Enum
 class LLMEnums(Enum):
     OPENAAI = "OPENAI"
     COHERE = "COHERE"
+
+
+class OpenAIEnums(Enum):
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
