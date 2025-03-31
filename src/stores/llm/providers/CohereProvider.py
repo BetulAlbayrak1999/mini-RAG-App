1d2fb013875165e46ab175c69a2e1b1f870830c8
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-03-31 12:53:24 +0300

    implementing Cohere methods that has been inherited from LLMInterface

--- src/stores/llm/providers/CohereProvider.py
+++ src/stores/llm/providers/CohereProvider.py
@@ -1,5 +1,5 @@
 from src.stores.LLMInterface import LLMInterface
-from src.stores.LLMEnums import CohereEnums
+from src.stores.LLMEnums import CohereEnums, DocumentTypeEnums
 import cohere
 import logging
 
@@ -27,3 +27,92 @@ class CohereProvider(LLMInterface):
         self.client = cohere.Client(api_key=self.api_key)
 
         self.logger = logging.getLogger(__name__)
+
+        # set_generation_model method
+        def set_generation_model(self, model_id: str):
+            self.generation_model_id = model_id
+
+        # set_embedding_model method
+        def set_embedding_model(self, model_id: str, embedding_size: int):
+            self.embedding_model_id = model_id
+            self.embedding_size = embedding_size
+
+        # process_text method
+        def process_text(self, text: str):
+            return text[: self.default_input_max_characters].strip()
+
+        # generate_text method
+        def generate_text(
+            self,
+            prompt: str,
+            chat_history: list = [],
+            max_output_tokens: int = None,
+            temperature: float = None,
+        ):
+
+            if not self.client:
+                self.logger.error("Cohere client was not set")
+                return None
+
+            if not self.generation_model_id:
+                self.logger.error("Generation model for Cohere was not set")
+                return None
+
+            max_output_tokens = (
+                max_output_tokens
+                if max_output_tokens
+                else self.default_generation_max_output_tokens
+            )
+            temperature = (
+                temperature if temperature else self.default_generation_temperature
+            )
+
+            response = self.client.chat(
+                model=self.generation_model_id,
+                chat_history=chat_history,
+                message=self.process_text(prompt),
+                temperature=temperature,
+                max_tokens=max_output_tokens,
+            )
+
+            if not response or not response.text:
+                self.logger.error("Error while generating text with Cohere")
+                return None
+
+            return response.text
+
+        # construct_prompt method
+        def construct_prompt(self, prompt: str, role: str):
+            return {
+                "role": role,
+                "text": self.process_text(prompt),
+            }
+
+        # embed_text method
+        def embed_text(self, text: str, document_type: str = None):
+            if not self.client:
+                self.logger.error("Cohere client was not set")
+                return None
+
+            if not self.embedding_model_id:
+                self.logger.error("Embedding model for Cohere was not set")
+                return None
+
+            input_type = CohereEnums.DOCUMENT
+            if document_type == DocumentTypeEnums.QUERY:
+                input_type = CohereEnums.QUERY
+
+            response = self.client.embed(
+                model=self.embedding_model_id,
+                texts=[self.process_text(text)],
+                input_type=input_type,
+                embedding_types=["float"],
+            )
+
+            if not response or not response.embeddings or not response.embeddings.float:
+                self.logger.error("Error while embedding text with Cohere")
+                return None
+
+            return response.embeddings.float[0]
+
+        
