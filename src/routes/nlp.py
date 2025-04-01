d5a38ed8ff5896700b3ea3bddce5737f20b7d9bd
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-04-01 21:44:40 +0300

    implemention get_project_index_info & search_index methods & fixing JSON Serialization error

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, APIRouter, status, Request
 from fastapi.responses import JSONResponse
-from routes.schemes.nlp import PushRequest
+from routes.schemes.nlp import PushRequest, SearchRequest
 from models import ProjectModel, ChunkModel
 from controllers import NLPController
 from models.enums import ResponseSignal
-
+import json
 import logging
 
 logger = logging.getLogger("uvicorn.error")
@@ -83,5 +83,75 @@ async def index_project(request: Request, project_id: str, push_request: PushReq
         content={
             "signal": ResponseSignal.INSERT_INTO_VECTORDB_SUCCESS.value,
             "inserted_items_count": inserted_items_count,
+        }
+    )
+
+
+@nlp_router.get("/index/info/{project_id}")
+async def get_project_index_info(request: Request, project_id: str):
+
+    project_model = await ProjectModel.create_instance(
+        db_client=request.app.db_client,
+    )
+
+    project = await project_model.get_or_create_project(
+        project_id=project_id,
+    )
+
+    nlp_controller = NLPController(
+        vectordb_client=request.app.vectordb_client,
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client,
+    )
+
+    collection_info = nlp_controller.get_vectordb_collection_info(
+        project=project,
+    )
+
+    return JSONResponse(
+        content={
+            "signal": ResponseSignal.VECTORDB_COLLECTION_RETRIEVED.value,
+            "collection_info": collection_info,
+        }
+    )
+
+
+@nlp_router.post("/index/search/{project_id}")
+async def search_index(
+    request: Request, project_id: str, search_request: SearchRequest
+):
+
+    project_model = await ProjectModel.create_instance(
+        db_client=request.app.db_client,
+    )
+
+    project = await project_model.get_or_create_project(
+        project_id=project_id,
+    )
+
+    nlp_controller = NLPController(
+        vectordb_client=request.app.vectordb_client,
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client,
+    )
+
+    results = nlp_controller.search_vectordb_collection(
+        project=project,
+        text=search_request.text,
+        limit=search_request.limit,
+    )
+
+    if not results:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.VECTORDB_SEARCH_ERROR.value,
+            },
+        )
+
+    return JSONResponse(
+        content={
+            "signal": ResponseSignal.VECTORDB_SEARCH_SUCCESS.value,
+            "results": results,
         }
     )
