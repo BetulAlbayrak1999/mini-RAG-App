a1f6255839c7e1c126686348d8a6f1547007b08a
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-04-02 14:23:36 +0300

    correcting generation_text to generate_text

--- src/stores/llm/LLMInterface.py
+++ src/stores/llm/LLMInterface.py
@@ -12,7 +12,7 @@ class LLMInterface(ABC):
         pass
 
     @abstractmethod
-    def generation_text(
+    def generate_text(
         self, prompt: str, chat_history: list=[], max_output_tokens: int=None, temperature: float = None
     ):
         pass
