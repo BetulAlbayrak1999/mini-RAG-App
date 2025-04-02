59d36aa21ae24a7f1052c981e3c94de2793b8d71
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-04-02 14:28:40 +0300

    implementing rag template for english language

---
+++ src/stores/llm/templates/locales/en/rag.py
@@ -0,0 +1,44 @@
+from string import Template
+
+
+###### RAG PROMPTS ######
+
+
+##### SYSTEM #####
+
+system_prompt = Template(
+    "\n".join(
+        [
+            "You are an assistant to generate a response for the user.",
+            "You will be provided by a set of documents associated with the user's query.",
+            "You have to generate a response based on the documents provided.",
+            "Ignore the documents that are not related to the user's query.",
+            "You can appologize to the user if you are not able to generate a response.",
+            "You have to generate the response in the same language as the user's query.",
+            "Be polite and respectful to the user.",
+            "Be precise and concise in your response. Avoid unneccessary information.",
+        ]
+    )
+)
+
+
+##### DOCUMENT #####
+document_prompt = Template(
+    "\n".join(
+        [
+            "## Document No: $doc_num",
+            "### Content: $chunk_text",
+        ]
+    )
+)
+
+
+##### FOOTER #####
+footer_prompt = Template(
+    "\n".join(
+        [
+            "Based only on the above documents, please generate an answer for the user.",
+            "## Answer: ",
+        ]
+    )
+)
