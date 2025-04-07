ffde682189a04878fd103a2694269fa0ce7ff31f
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-04-07 18:25:29 +0300

    fixing the issue with asset_uuid column's value properties

--- src/models/db_schemes/minirag/schemes/asset.py
+++ src/models/db_schemes/minirag/schemes/asset.py
@@ -9,7 +9,9 @@ import uuid
 class Asset(SQLAlchemyBase):
     __tablename__ = "assets"
     asset_id = Column(Integer, primary_key=True, autoincrement=True)
-    asset_uuid = Column(String, nullable=False)
+    asset_uuid = Column(
+        UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False
+    )
     asset_type = Column(String, nullable=False)
     asset_name = Column(String, nullable=False)
     asset_size = Column(Integer, nullable=False)
@@ -26,7 +28,7 @@ class Asset(SQLAlchemyBase):
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
 
     project = relationship("Project", back_populates="assets")
-    chunks = relationship("DataChunk", back_populates="assets")
+    chunks = relationship("DataChunk", back_populates="asset")
 
     __table_args__ = (
         Index("ix_asset_project_id", asset_project_id),
