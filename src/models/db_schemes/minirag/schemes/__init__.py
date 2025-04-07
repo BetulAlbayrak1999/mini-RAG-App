e9cc376a9d6288a4a49a9d27b9e2bb41d0c2da94
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-04-07 18:22:22 +0300

    updating "import" part

--- src/models/db_schemes/minirag/schemes/__init__.py
+++ src/models/db_schemes/minirag/schemes/__init__.py
@@ -1,4 +1,4 @@
-from  schemes.minirag_base import SQLAlchemyBase
-from  schemes.asset import Asset
-from  schemes.project import Project
-from  schemes.datachunk import DataChunk, RetrievedDocument
+from  .minirag_base import SQLAlchemyBase
+from  .asset import Asset
+from  .project import Project
+from  .datachunk import DataChunk, RetrievedDocument
